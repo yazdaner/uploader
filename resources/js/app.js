20330a79c8e573b3be9c7b853c65a8dcfc00c144
yazdan <yazdaner8@gmail.com>
2023-10-13 21:57:47 +0330

    v1.0.1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+
+import App from './components/App.vue';
+import VueAxios from 'vue-axios';
+
+const app = createApp(App);
+
+app.mount('#app');
+app.use(axios,VueAxios);
+
+
+
+
+
+
+
+
---
+++ resources/js/components/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>uploader</div>
+</template>
