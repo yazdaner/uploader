4462bce42d654471a15484e8d58a9440513d9f8f
yazdan <yazdaner8@gmail.com>
2024-01-30 23:41:24 +0330

    create chunks method 1.1.1

--- resources/js/components/UploadComponent.vue
+++ resources/js/components/UploadComponent.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { ref } from "vue";
-const step = 1;
+const step = ref(1);
 const file = ref();
 const formatError = ref(false);
+const stop = ref(false);
+const selectedFileIndex = ref(null);
 async function select(event) {
     file.value = event.target.files[0];
     await checkFileTypes();
-    console.log(1);
+    createChunks();
 }
 function checkFileTypes() {
     const types = ["mp4", "mov", "wvm", "flv", "avi", "mkv"];
@@ -19,10 +21,28 @@ function checkFileTypes() {
     }
     error ? (formatError.value = true) : (formatError.value = false);
 }
+const chunks = ref([]);
+function createChunks() {
+    if (formatError.value == false) {
+        let size = 500000;
+        let chunksLength = Math.ceil(file.value.size / size);
+        for (let i = 0; i < chunksLength; i++) {
+            chunks.value.push(
+                file.value.slice(
+                    i * size,
+                    Math.min(i * size + size, file.value.size),
+                    file.value.type
+                )
+            );
+        }
+        step.value = 2
+    }
+    selectedFileIndex.value = 0
+}
 </script>
 
 <template>
-    <div v-if="step == 1">
+    <div v-if="step.value == 1">
         <div
             class="upload-box bg-white rounded cursor-pointer text-center shadow mx-auto w-50 mt-150 p-5"
         >
